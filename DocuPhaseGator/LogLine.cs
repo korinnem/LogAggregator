32d2eb16234bb859b75e0ed2c7d7642c5d5403f0
korinnem <martinkorinne89@gmail.com>
2018-12-11 12:17:08 -0500

    patch-1_12-11-2018
    Code changes to fix the following bugs: 
    - certain logs appeared to be missing information because null values were not being replaced with "no data available"
    - html configurations in certain logs were causing exceptions when trying to run the gator
    - line breaks within log lines were causing new logline logic to fire when the text should have been added to the previous logline
    - support for archived logs
    - Log Location now pulls consistently

---
+++ DocuPhaseGator/LogLine.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LogIngest
+{
+    public class LogLine
+    {
+        DateTime logTime;
+        String logSource;
+        String logLevel;
+        String logCode;
+        String logLocation;
+        String logText;
+
+
+        public LogLine(string log, string source)
+        {
+            logSource = setLogSource(source);
+            logLevel = setlogLevel(log);
+            logTime = doDateTimeConversion(log);
+            logCode = parseLogCode(log);
+            logLocation = parseLogLocation(log);
+            logText = parseLogText(log);
+        }
+
+        public String setLogSource(String source)
+        {
+            String[] parts = source.Split('.');
+            String logSource = parts[0];
+            return logSource;
+        }
+
+        public DateTime doDateTimeConversion(string logString)
+        {
+            DateTime date = new DateTime();
+            date = Convert.ToDateTime(logString.Substring(0, 23));
+            return date;
+        }
+
+        public String setlogLevel(String logString)
+        {
+            var text = logString.Split(new[] { ' ' }, 4);
+            String logLevel = text[2].ToString();
+            return logLevel;
+        }
+
+        public String parseLogCode(String logString)
+        {
+            String code = "";
+            var reg = new Regex(@"\[(.*?)\]");
+            var matches = reg.Matches(logString);
+            code = matches[0].ToString();
+            char[] charsToTrim = { '[', ']' };
+            code = code.Trim(charsToTrim);
+            return code;
+        }
+
+        public String parseLogLocation(String logString)
+        {
+            String logLocation = "";
+            String[] delimiterChars = { "-", "   " };
+            String[] results = logString.Split(delimiterChars, System.StringSplitOptions.RemoveEmptyEntries);
+            if (results.Length > 1)
+            {
+                logLocation = results[1].ToString().Trim();
+            }
+            else
+            {
+                logLocation = "Error Parsing Log Location";
+            }
+
+            return logLocation;
+        }
+
+        public String parseLogText(String logString)
+        {
+            String logText = "";
+            var text = logString.Split(new[] { '-' }, 2);
+            if (text.Length > 1)
+            {
+                logText = text[1].ToString();
+
+            }
+
+            if (logText == "" || logText == null)
+            {
+                logText = "No Additional Information found in log";
+            }
+            return logText;
+        }
+
+        public String getLog()
+        {
+            String logLine = (logTime + " |logSource=" + logSource + " |logLevel=" +
+                logLevel + " |logCode=" + logCode + " |logLocation=" + logLocation +
+                " |logText=" + logText);
+
+            return logLine;
+        }
+
+        public DateTime getLogTime()
+        {
+            return logTime;
+        }
+
+        public String getLogSource()
+        {
+            return logSource;
+        }
+
+        public String getLogLevel()
+        {
+            return logLevel;
+        }
+
+        public String getLogCode()
+        {
+            return logCode;
+        }
+
+        public String getLogLocation()
+        {
+            return logLocation;
+        }
+
+        public String getLogText()
+        {
+            return logText;
+        }
+
+    }
+
+
+}
---
+++ DocuPhaseGator/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace LogIngest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String logDir = @"C:\ProgramData\Docuphase\tracelogs";
+            List<LogLine> allLogLines = new List<LogLine>();
+            allLogLines = readLogs(logsExist(logDir));
+            List<LogLine> sortedList = sortList(allLogLines);
+            writeLogs(sortedList);
+
+        }
+
+
+
+        // --- Checks the default location for logs that are accessible ---
+        static List<String> logsExist(String logDir)
+        {
+            List<String> logList = new List<String>();
+
+            string[] getFiles = Directory.GetFiles(logDir);
+
+            foreach (string name in getFiles)
+            {
+                // --- strip out the path to the file from the filename --- 
+                // TODO: change this so that it works in the case of a custom logPath
+                // instead of hard coding the removal of the path
+                string logName = name.Substring(35);
+
+                // --- add filename to the list ---
+                logList.Add(logName);
+            }
+
+            if (logList.Count() > 0)
+            {
+                foreach (var logName in logList)
+                {
+                    String logTargetPath = @"C:\tracelogs\";
+
+                    String logSourceDir = logDir + '\\' + logName;
+                    String logTargetDir = logTargetPath + logName;
+
+                    if (!System.IO.Directory.Exists(logTargetPath))
+                    {
+                        System.IO.Directory.CreateDirectory(logTargetPath);
+                    }
+                    System.IO.File.Copy(logSourceDir, logTargetDir, true);
+                }
+
+                return logList;
+            }
+            else
+            {
+                Console.WriteLine("There are no log files in the tracelogs folder");
+                return null;
+            }
+        }
+
+        // --- use the logList to read all logs into a single List<LogLine> --- 
+        static List<LogLine> readLogs(List<String> logList)
+        {
+            String logHolding = "";
+            String logLocation = "";
+            List<LogLine> listOfLogs = new List<LogLine>();
+            for (int i = 0; i < logList.Count; i++)
+            {
+                logLocation = (@"C:\tracelogs\" + logList[i]);
+                foreach (string line in File.ReadLines(logLocation))
+                {
+                    String trimLine = line.Trim();
+                    if (trimLine == null || trimLine == "  " || trimLine == "" || trimLine == " " || trimLine == "   ")
+                    {
+                        //do nothing
+                    }
+                    else if (trimLine != null)
+                    {
+                        String regexMatch = @"\d{4}";
+                        if (trimLine.Length > 3)
+                        {
+                            if (Regex.IsMatch(trimLine.Substring(0, 4), regexMatch))
+                            {
+                                if (logHolding != "")
+                                {
+                                    LogLine pushLog = new LogLine(logHolding, logList[i]);
+                                    listOfLogs.Add(pushLog);
+                                    logHolding = "";
+
+                                }
+                                logHolding = trimLine;
+                            }
+                            else
+                            {
+                                logHolding += "\n\t" + trimLine;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        LogLine pushLog = new LogLine(logHolding, logList[i]);
+                        listOfLogs.Add(pushLog);
+                        logHolding = "";
+
+                    }
+                }
+            }
+            return listOfLogs;
+        }
+
+        static List<LogLine> sortList(List<LogLine> allLogs)
+        {
+            List<LogLine> sorted = allLogs.OrderBy(logLine => logLine.getLogTime()).ToList();
+            return sorted;
+        }
+
+        static void writeLogs(List<LogLine> allLogs)
+        {
+            using (TextWriter tw = new StreamWriter(@"C:\tracelogs\chronologs.txt"))
+            {
+                for (int i = 0; i < allLogs.Count; i++)
+                {
+                    tw.WriteLine(allLogs[i].getLog());
+                }
+
+                tw.Close();
+            }
+
+        }
+    }
+}
